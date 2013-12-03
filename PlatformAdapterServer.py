1bc2997f574a62a059a46334022aaa69a7f6e3bd
avinassh <avinashsajjan@gmail.com>
2013-12-03 16:28:49 +0530

    added platform adapter server

---
+++ PlatformAdapterServer.py
@@ -0,0 +1,54 @@
+# Author : Avinash <a@vlabs.ac.in>
+# Organization : VLEAD, Virtual-Labs
+
+import time
+import BaseHTTPServer
+import urlparse
+import urllib 
+import SocketServer
+import threading
+
+
+from PlatformAdapter import *
+from VMSpec import VMSpec
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 8089
+
+
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_GET(self):
+        """ Respond to a GET request. """   
+        return
+            
+
+    def do_POST(self):
+        """ Respond to a POST request """
+
+        length = int(self.headers['Content-Length'])
+        post_data = urlparse.parse_qs(self.rfile.read(length).decode('utf-8'))        
+        
+        vm_spec = VMSpec(post_data)
+        platform_adapter = CentOSVZAdapter()
+        platform_adapter.create_VM("99100", vm_spec)
+
+        return platform_adapter
+
+class ThreadedHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    """ Handle requests in a separate thread. """
+    pass
+
+if __name__ == '__main__':
+    httpd = ThreadedHTTPServer((HOST_NAME, PORT_NUMBER), Handler)
+    try:
+        httpd.serve_forever()
+        print "Server Started - %s:%s with the thread :%s" % (HOST_NAME, PORT_NUMBER, server_thread.name)
+    except KeyboardInterrupt:
+        httpd.server_close()
+    print "Server Stopped - %s:%s" % (HOST_NAME, PORT_NUMBER)
