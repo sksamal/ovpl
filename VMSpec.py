2ca30018814968af9ae7b85f355a6556a90bc03c
avinassh <avinashsajjan@gmail.com>
2013-12-03 16:32:53 +0530

    modified VMSpec.py to receive vm spec dictionary as paramter for constructor

--- VMSpec.py
+++ VMSpec.py
@@ -5,11 +5,11 @@
 
 class VMSpec:
     """ Represents build and installation specifications of a VM. """
-    def __init__(self):
+    def __init__(self, specs):
         # Temporarily setting defaults
-        self.lab_ID = "test99"
-        self.os = "Ubuntu"
-        self.os_version = "12.04"
-        self.ram = "256M"
-        self.swap = "512M"
-        self.diskspace = "2G"
+        self.lab_ID = specs['lab_ID'][0] #"test99"
+        self.os = specs['os'][0] #"Ubuntu"
+        self.os_version = specs['os_version'][0] #"12.04"
+        self.ram = specs['ram'][0] #"256M"
+        self.swap = specs['swap'][0] #"512M"
+        self.diskspace = specs['diskspace'][0] #"2G"
