0f20d75b2c3e822862984a8e4a7cd8e9c002a317
avinassh <avinashsajjan@gmail.com>
2013-12-04 11:11:30 +0530

    changed VMSpec.py file accoring to new POST data format

--- VMSpec.py
+++ VMSpec.py
@@ -7,9 +7,9 @@ class VMSpec:
     """ Represents build and installation specifications of a VM. """
     def __init__(self, specs):
         # Temporarily setting defaults
-        self.lab_ID = specs['lab_ID'][0] #"test99"
-        self.os = specs['os'][0] #"Ubuntu"
-        self.os_version = specs['os_version'][0] #"12.04"
-        self.ram = specs['ram'][0] #"256M"
-        self.swap = specs['swap'][0] #"512M"
-        self.diskspace = specs['diskspace'][0] #"2G"
+        self.lab_ID = specs['lab_ID'] #"test99"
+        self.os = specs['os'] #"Ubuntu"
+        self.os_version = specs['os_version'] #"12.04"
+        self.ram = specs['ram'] #"256M"
+        self.swap = specs['swap'] #"512M"
+        self.diskspace = specs['diskspace'] #"2G"
