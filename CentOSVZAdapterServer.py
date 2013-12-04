7ed4f2a37ed896e9f79aab508a48cf93f757b63c
Chandan Kumar <chandan.gupta@gmail.com>
2013-12-04 15:42:50 +0530

    Changed module name

--- CentOSVZAdapterServer.py
+++ CentOSVZAdapterServer.py
@@ -8,7 +8,6 @@ import urllib
 import SocketServer
 import threading
 
-
 import CentOSVZAdapter
 from VMSpec import VMSpec
 
@@ -16,33 +15,36 @@ HOST_NAME = 'localhost'
 PORT_NUMBER = 8089
 
 
-class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
-
+class CentOSVZAdapterHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """  """
     def do_HEAD(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
     def do_GET(self):
-        """ Respond to a GET request. """   
-        return
-            
+        pass
 
     def do_POST(self):
-        """ Respond to a POST request """
-
         length = int(self.headers['Content-Length'])
-        post_data dict(urlparse.parse_qsl(self.rfile.read(length)))
+        post_data = dict(urlparse.parse_qsl(self.rfile.read(length)))
         vm_spec = VMSpec(post_data)
         #platform_adapter = CentOSVZAdapter()
-        return CentOSVZAdapter.create_VM("99100", vm_spec)    
+        response = CentOSVZAdapter.create_vm("99100", vm_spec)
+        #CentOSVZAdapter.destroy_vm("99100")
+        self.wfile.write(response)
+        return    
+
 
 class ThreadedHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
     """ Handle requests in a separate thread. """
     pass
 
+
 if __name__ == '__main__':
-    httpd = ThreadedHTTPServer((HOST_NAME, PORT_NUMBER), Handler)
+    # httpd = ThreadedHTTPServer((HOST_NAME, PORT_NUMBER), CentOSVZAdapterHandler)
+    httpd = BaseHTTPServer.HTTPServer((HOST_NAME, PORT_NUMBER), CentOSVZAdapterHandler)
+
     try:
         httpd.serve_forever()
         print "Server Started - %s:%s with the thread :%s" % (HOST_NAME, PORT_NUMBER, server_thread.name)
